0487e5e289a55bd23db4b74dcf80a2e8f8eb969a
pinksashimi <aidanurdini19@gmail.com>
2025-01-10 02:47:29 +0800

    Updates

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the homepage link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Welcome to Visit Penang/i);
-  expect(linkElement).toBeInTheDocument();
-});
